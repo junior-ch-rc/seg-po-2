24f91a8582efe68fdede57ecb7eb0d300e201769
Manoel Ricardo <manoelricardo38@gmail.com>
2023-04-29 14:55:07 -0300

    alteracao de nomes de colunas na impl do banco e funcoes de investimento

--- manoel-pratica-off2/src/servidor/BankImpl.java
+++ manoel-pratica-off2/src/servidor/BankImpl.java
@@ -9,7 +9,6 @@ import common.MeuHash;
 public class BankImpl {
     
     private Connection conn;
-    private int accountNumber;
     
     public BankImpl(String username, String password, String databaseURL) throws SQLException, ClassNotFoundException {
     	Class.forName("org.postgresql.Driver");
@@ -55,41 +54,67 @@ public class BankImpl {
     }
     
     // Saque
-    protected void withdraw(int amount) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET balance = balance - ? WHERE account_number = ?");
+    protected void withdraw(int accountNumber, int amount) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo - ? WHERE id = ?");
         stmt.setInt(1, amount);
         stmt.setInt(2, accountNumber);
         stmt.executeUpdate();
     }
     
     // Depósito
-    protected void deposit(int amount) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET balance = balance + ? WHERE account_number = ?");
+    protected void deposit(int accountNumber, int amount) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo + ? WHERE id = ?");
         stmt.setInt(1, amount);
         stmt.setInt(2, accountNumber);
         stmt.executeUpdate();
     }
     
     // Transferência
-    protected void transfer(int accountTo, int amount) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET balance = balance - ? WHERE account_number = ?");
+    protected void transfer(int accountOrigin, int accountTo, int amount) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo - ? WHERE id = ?");
         stmt.setInt(1, amount);
-        stmt.setInt(2, accountNumber);
+        stmt.setInt(2, accountOrigin);
         stmt.executeUpdate();
         
-        stmt = conn.prepareStatement("UPDATE accounts SET balance = balance + ? WHERE account_number = ?");
+        stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo + ? WHERE id = ?");
         stmt.setInt(1, amount);
         stmt.setInt(2, accountTo);
         stmt.executeUpdate();
     }
     
     // Saldo
-    protected int getBalance() throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("SELECT balance FROM accounts WHERE account_number = ?");
+    protected int getBalance(int accountNumber) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT saldo FROM saldo WHERE id = ?");
         stmt.setInt(1, accountNumber);
         ResultSet rs = stmt.executeQuery();
         rs.next();
         return rs.getInt("balance");
+    }
+    
+    // Investir em Poupanca
+    protected void investSavings(int accountNumber, int amount) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo - ? WHERE id = ?");
+        stmt.setInt(1, amount);
+        stmt.setInt(2, accountNumber);
+        stmt.executeUpdate();
+        
+        stmt = conn.prepareStatement("UPDATE accounts SET saldo_poupanca = saldo_poupanca + ? WHERE id = ?");
+        stmt.setInt(1, amount);
+        stmt.setInt(2, accountNumber);
+        stmt.executeUpdate();
+    }
+    
+    // Investir em Renda Fixa
+    protected void investFixedIncome(int accountNumber, int amount) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE accounts SET saldo = saldo - ? WHERE id = ?");
+        stmt.setInt(1, amount);
+        stmt.setInt(2, accountNumber);
+        stmt.executeUpdate();
+        
+        stmt = conn.prepareStatement("UPDATE accounts SET saldo_rendaFixa = saldo_rendaFixa + ? WHERE id = ?");
+        stmt.setInt(1, amount);
+        stmt.setInt(2, accountNumber);
+        stmt.executeUpdate();
     }
     
 }
